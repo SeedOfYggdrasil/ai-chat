1cc745574434f83000913a59aa833860655036a6
SeedOfYggdrasil <115541458+SeedOfYggdrasil@users.noreply.github.com>
2025-03-08 02:30:49 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,8 +10,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3001/api",
+      "/api/": {
+        target: "http://localhost:3001/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
