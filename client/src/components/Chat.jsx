940d537f944e144e03521b5c8667b6f94c489197
seedofyggdrasil <booking.pariah@gmail.com>
2025-04-16 07:58:46 -0400

    Transition to Gemini

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,3 +1,5 @@
+// @c/Chat.jsx
+
 import { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
@@ -18,7 +20,7 @@ const Chat = () => {
     dispatch,
   } = useAPI();
 
-  const models = ["llama3.1", "codellama", "gemma", "mistral"];
+  const models = ["Gemini-2.0-Flash"];
   const chatWindowRef = useRef(null);
 
   useEffect(() => {
